4ae3b72605b74f0233204c9d74f6844f0a261842
Paradox <73344759+Hello-day@users.noreply.github.com>
2024-06-16 14:50:32 +0800

    the main structure file

---
+++ mynet.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# MobileNetV3 components
+
+class hswish(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu6 = nn.ReLU6(inplace=True)
+
+    def forward(self, x):
+        out = x * self.relu6(x + 3) / 6
+        return out
+
+
+class hsigmoid(nn.Module):
+    def forward(self, x):
+        out = F.relu6(x + 3, inplace=True) / 6
+        return out
+
+
+class EcaModule(nn.Module):
+    def __init__(self, in_channels, k_size=3):
+        super(EcaModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+
+
+class Block(nn.Module):
+    '''expand + depthwise + pointwise'''
+
+    def __init__(self, kernel_size, in_size, expand_size, out_size, act, se, stride):
+        super(Block, self).__init__()
+        self.stride = stride
+
+        self.conv1 = nn.Conv2d(in_size, expand_size, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(expand_size)
+        self.act1 = act(inplace=True)
+
+        self.conv2 = nn.Conv2d(expand_size, expand_size, kernel_size=kernel_size, stride=stride,
+                               padding=kernel_size // 2, groups=expand_size, bias=False)
+        self.bn2 = nn.BatchNorm2d(expand_size)
+        self.act2 = act(inplace=True)
+       # self.se = EcaModule(expand_size) if se else nn.Identity()
+
+        self.conv3 = nn.Conv2d(expand_size, out_size, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_size)
+
+        self.skip = None
+        if stride == 1 and in_size != out_size:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_size, out_size, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_size)
+            )
+
+        if stride == 2 and in_size != out_size:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels=in_size, out_channels=in_size, kernel_size=3, groups=in_size, stride=2, padding=1,
+                          bias=False),
+                nn.BatchNorm2d(in_size),
+                nn.Conv2d(in_size, out_size, kernel_size=1, bias=True),
+                nn.BatchNorm2d(out_size)
+            )
+
+        if stride == 2 and in_size == out_size:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels=in_size, out_channels=out_size, kernel_size=3, groups=in_size, stride=2,
+                          padding=1, bias=False),
+                nn.BatchNorm2d(out_size)
+            )
+
+    def forward(self, x):
+        skip = x
+
+        out = self.act1(self.bn1(self.conv1(x)))
+        out = self.act2(self.bn2(self.conv2(out)))
+#        out = self.se(out)
+        out = self.bn3(self.conv3(out))
+
+        if self.skip is not None:
+            skip = self.skip(skip)
+        return out + skip
+
+
+# UNet with MobileNetV3 downsampling
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU6(inplace=True)
+            #hswish()#激活函数选择
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Block(3, 64, 128, 128, nn.ReLU6, True, 2)
+        self.down2 = Block(3, 128, 256, 256, nn.ReLU6, True, 2)
+        self.down3 = Block(3, 256, 512, 512,nn.ReLU6, True, 2)
+        self.down4 = Block(3, 512, 512, 512, nn.ReLU6, True, 2)
+        self.up1 = Up(1024, 256, bilinear)
+        self.up2 = Up(512, 128, bilinear)
+        self.up3 = Up(256, 64, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+
+if __name__ == '__main__':
+    net = UNet(n_channels=3, n_classes=1)
+    print(net)
